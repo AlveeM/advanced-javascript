230041b42a62c7c758df88a83d2e98ea30614afe
Alvee Akand <AlveeM@users.noreply.github.com>
2019-10-06 16:44:00 +0600

    updated memory-leak.js

--- js-foundation-1/memory-leak.js
+++ js-foundation-1/memory-leak.js
@@ -2,3 +2,19 @@ let arr = [];
 for (let i = 5; i > 1; i++) {
     arr.push(i - 1);
 }
+
+// Three Common Memory Leaks
+
+// Global Variables
+var a = 1;
+var b = 2;
+var c = 3;
+
+// Event Listeners
+var element = document.getElementById('button');
+element.addEventListener('click', onClick());
+
+// setInterval
+setInterval(() => {
+    // referencing objects
+})
