4b2a60952c395e3ad795754e1a47d8f526cac53a
Alvee Akand <AlveeM@users.noreply.github.com>
2019-10-13 11:36:09 +0600

    added OOP3

---
+++ object-oriented-programming/OOP3.js
@@ -0,0 +1,25 @@
+// Constructor Functions
+function Elf(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+}
+
+Elf.prototype.attack = function() {
+    return 'attack with ' + this.weapon;
+}
+
+const peter = new Elf('Peter', 'stones');
+console.log(peter.name);
+console.log(peter.attack());
+
+const sam = new Elf('Sam', 'fire');
+console.log(sam.name);
+console.log(sam.attack());
+
+// Using the Function constructor
+const elf1 = new Function('name', 'weapon',
+    `this.name = name;
+    this.weapon = weapon;`)
+
+const sarah = new elf1('Sarah', 'fireworks');
+console.log(sarah);
