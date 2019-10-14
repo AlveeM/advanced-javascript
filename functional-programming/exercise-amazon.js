976f739cdbd31ab790249bdc5a7ca82a910e3324
Alvee Akand <AlveeM@users.noreply.github.com>
2019-10-14 16:34:02 +0600

    added FP exercise

--- functional-programming/exercise-amazon.js
+++ functional-programming/exercise-amazon.js
@@ -6,12 +6,53 @@ const user = {
     purchases: []
   }
 
+let amazonHistory = [];
+const compose = (f, g) => (...args) => f(g(...args));
+
+console.log(
+  purchaseItem(
+    emptyCart,
+    buyItem,
+    applyTaxToItems,
+    addItemToCart
+  ) (user, {name: 'laptop', price: 200})
+)
+
+function purchaseItem(...fns) {
+  return fns.reduce(compose)
+}
+
+function addItemToCart(user, item) {
+  amazonHistory.push(user);
+  const updatedCart = user.cart.concat(item);
+  return Object.assign({}, user, { cart: updatedCart });
+}
+
+function applyTaxToItems(user) {
+  amazonHistory.push(user);
+  const {cart} = user;
+  const taxRate = 1.03;
+  const updatedCart = cart.map(item => {
+    return {
+      name: item.name,
+      price: item.price * taxRate
+    }
+  })
+  return Object.assign({}, user, { cart: updatedCart });
+}
+
+function buyItem(user) {
+  amazonHistory.push(user);
+  return Object.assign({}, user, { purchases: user.cart });
+}
+
+function emptyCart(user) {
+  amazonHistory.push(user);
+  return Object.assign({}, user, { cart: [] });
+}
+
 //Implement a cart feature:
 // 1. Add items to cart.
 // 2. Add 3% tax to item in cart
 // 3. Buy item: cart --> purchases
 // 4. Empty cart
-
-//Bonus:
-// accept refunds.
-// Track user history.
